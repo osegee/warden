2815d5a7c2134c8bd8ea64571b6a993cd525a579
osegee <osegiegodsent59@gmail.com>
2026-08-17 14:31:52 +0100

    user and otp model

---
+++ src/models/otp.js
@@ -0,0 +1,37 @@
+import { pool } from "../config/db.js";
+
+const createOtp = async (user_id, code, purpose, expires_at) => {
+  const result = await pool.query(
+    `INSERT INTO otp_codes (user_id, code, purpose, expires_at) VALUES($1, $2, $3, $4) RETURNING * `,
+    [user_id, code, purpose, expires_at],
+  );
+  return result.rows[0];
+};
+const findOtpByUserAndPurpose = async (user_id, purpose) => {
+  const result = await pool.query(
+    `SELECT * FROM otp_codes WHERE user_id = $1 AND purpose = $2,`,
+    [user_id, purpose],
+  );
+  return result.rows[0];
+};
+const markOtpAsUsed = async (otp_id) => {
+  const result = await pool.query(
+    `UPDATE otp_codes SET is_used = true WHERE id = $1`,
+    [otp_id],
+  );
+  return result.rows[0];
+};
+const incrementOtpAttempts = async (otp_id) => {
+  const result = await pool.query(
+    `UPDATE otp_codes SET attempts = attempts + 1 WHERE id = $1`,
+    [otp_id],
+  );
+  return result.rows[0];
+};
+
+export {
+  createOtp,
+  findOtpByUserAndPurpose,
+  markOtpAsUsed,
+  incrementOtpAttempts,
+};
---
+++ src/models/user.js
@@ -0,0 +1,20 @@
+import { pool } from "../config/db.js";
+
+const findUserByEmail = async (email) => {
+  const result = await pool.query(`SELECT * FROM users WHERE email = $1`, [
+    email.toLowerCase(),
+  ]);
+
+  return result.rows[0];
+};
+
+const createUser = async (username, email, password) => {
+  const result = await pool.query(
+    `INSERT INTO users (username, email, password) VALUES($1, $2, $3) RETURNING id, username, email, is_verified, created_at`,
+    [username, email.toLowerCase(), password],
+  );
+
+  return result.rows[0];
+};
+
+export { findUserByEmail, createUser };
