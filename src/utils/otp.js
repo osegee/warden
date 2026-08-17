df4f14a07d3303b71353dd4c3433991fa1deac17
osegee <osegiegodsent59@gmail.com>
2026-08-17 14:29:53 +0100

    generate and hash otp

--- src/utils/otp.js
+++ src/utils/otp.js
@@ -1,10 +1,11 @@
 import crypto from "crypto";
 
-// Generates a 6-digit numeric OTP as a string, e.g. "042817"
-export function generateOtp() {
+const generateOtp = () => {
   return crypto.randomInt(100000, 999999).toString();
-}
+};
 
-export function hashOtp(otp) {
+const hashOtp = (otp) => {
   return crypto.createHash("sha256").update(otp).digest("hex");
-}
+};
+
+export { generateOtp, hashOtp };
