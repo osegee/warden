a81904409e93c7524b9a300bcea84ccf0372e705
osegee <osegiegodsent59@gmail.com>
2026-08-17 14:29:06 +0100

    email helper function

---
+++ src/utils/email.js
@@ -0,0 +1,27 @@
+import nodemailer from "nodemailer";
+
+const transport = nodemailer.createTransport({
+  host: process.env.BREVO_SMTP_HOST,
+  port: process.env.SMTP_PORT,
+  auth: {
+    user: process.env.SENDER_EMAIL,
+    pass: process.env.BREVO_API_KEY,
+  },
+});
+
+const sendOtpEmail = async (email, otp) => {
+  await transport.sendMail({
+    from: `Warden ${process.env.EMAIL_FROM}`,
+    to: email,
+    subject: "Your warden OTP code",
+    html: `
+        <h2>Email Verification</h2>
+        <p>Your OTP code is:</p>
+        <h1>${otp}</h1>
+        <p>This code expires in 10 minutes.</p>
+        <p>If you didn't request this, ignore this email</p>
+    `,
+  });
+};
+
+export { sendOtpEmail };
