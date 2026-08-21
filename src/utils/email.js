eb7d50f502ea6743ad8b99b7c8f2b233b77eb544
osegee <osegiegodsent59@gmail.com>
2026-08-21 10:47:07 +0100

    fix: replace nodemailer SMTP with Brevo HTTP API to resolve ETIMEDOUT on Render

--- src/utils/email.js
+++ src/utils/email.js
@@ -1,28 +1,58 @@
-import nodemailer from "nodemailer";
+// import nodemailer from "nodemailer";
 
-const transport = nodemailer.createTransport({
-  host: process.env.BREVO_SMTP_HOST,
-  port: process.env.SMTP_PORT,
-  secure: true, // true for 465, false for other portss
-  auth: {
-    user: process.env.SENDER_EMAIL,
-    pass: process.env.BREVO_API_KEY,
-  },
-});
+// const transport = nodemailer.createTransport({
+//   host: process.env.BREVO_SMTP_HOST,
+//   port: process.env.SMTP_PORT,
+//   secure: true, // true for 465, false for other portss
+//   auth: {
+//     user: process.env.SENDER_EMAIL,
+//     pass: process.env.BREVO_API_KEY,
+//   },
+// });
+
+// const sendOtpEmail = async (email, otp) => {
+//   await transport.sendMail({
+//     from: `Warden ${process.env.EMAIL_FROM}`,
+//     to: email,
+//     subject: "Your warden OTP code",
+//     html: `
+//         <h2>Email Verification</h2>
+//         <p>Your OTP code is:</p>
+//         <h1>${otp}</h1>
+//         <p>This code expires in 10 minutes.</p>
+//         <p>If you didn't request this, ignore this email</p>
+//     `,
+//   });
+// };
+
+// export { sendOtpEmail };
+
+import * as brevo from "@getbrevo/brevo";
+
+const apiInstance = new brevo.TransactionalEmailsApi();
+apiInstance.setApiKey(
+  brevo.TransactionalEmailsApiApiKeys.apiKey,
+  process.env.BREVO_API_KEY,
+);
 
 const sendOtpEmail = async (email, otp) => {
-  await transport.sendMail({
-    from: `Warden ${process.env.EMAIL_FROM}`,
-    to: email,
-    subject: "Your warden OTP code",
-    html: `
+  const sendSmtpEmail = new brevo.SendSmtpEmail();
+
+  sendSmtpEmail.subject = "Your Warden OTP Code";
+  sendSmtpEmail.to = [{ email: email }];
+  sendSmtpEmail.sender = {
+    email: process.env.SENDER_EMAIL,
+    name: "Warden",
+  };
+  sendSmtpEmail.htmlContent = `
         <h2>Email Verification</h2>
         <p>Your OTP code is:</p>
         <h1>${otp}</h1>
         <p>This code expires in 10 minutes.</p>
-        <p>If you didn't request this, ignore this email</p>
-    `,
-  });
+        <p>If you didn't request this, ignore this email.</p>
+    `;
+
+  await apiInstance.sendTransacEmail(sendSmtpEmail);
 };
 
 export { sendOtpEmail };
