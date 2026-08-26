403ff2e6ee190cb200625f98b0699f8a13d92411
osegee <osegiegodsent59@gmail.com>
2026-08-26 21:26:00 +0100

    validation fix

--- src/middlewares/validate.middleware.js
+++ src/middlewares/validate.middleware.js
@@ -1,15 +1,19 @@
 const validate = (schema) => {
-  return (req, res, next) => {
-    const result = schema.safeParse(req.body);
-    if (!result.success) {
-      return res.status(400).json({
-        success: false,
-        message: result.error.issues.map((err) => err.message),
-      });
-    }
-    req.body = result.data;
-    next();
-  };
+  try {
+    return (req, res, next) => {
+      const result = schema.safeParse(req.body);
+      if (!result.success) {
+        return res.status(400).json({
+          success: false,
+          message: result.error.issues.map((err) => err.message),
+        });
+      }
+      req.body = result.data;
+      next();
+    };
+  } catch (error) {
+    next(error);
+  }
 };
 
 export { validate };
